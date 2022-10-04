9b028d1830c2e581713af22da8ded6cd4b882740
prabhjot <prabhjot@webkorps.com>
2022-10-05 00:10:52 +0530

    fixed pr issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
+    Pagy::DEFAULT[:items] = 9
     @q = Link.ransack(params[:q])
-    @links = @q.result(distinct: true)
+    @pagy,@links = pagy(@q.result)
   end
 
   def new
@@ -26,15 +27,33 @@ class HomeController < ApplicationController
   end
 
   def comment_create
-    @link = Link.find(params[:comment][:commentable])
-    @comment = Comment.new(params_comment)
+    @link = Link.find_by(id: params[:comment][:commentable])
+    @comment = current_user.comments.new(params_comment)
     @comment.commentable = @link
-    @comment.user = current_user
     @comment.save
-    flash[:errors] = 'error creating comment' if @comment.errors.any?
+    flash[:errors] = @comment.errors.full_message if @comment.errors.any?
     redirect_to flash[:back]
   end
 
+  def likeable
+    @link = Link.find(params[:id])
+    if params[:bool] == 'true'
+      if current_user.voted_for? @link
+        @link.unliked_by current_user
+      else
+        @link.liked_by current_user 
+      end
+    elsif params[:bool] == 'false'
+      if current_user.voted_for? @link
+        @link.undisliked_by current_user
+      else
+        @link.disliked_by current_user 
+      end
+    end
+    respond_to do |format|
+      format.turbo_stream { render partial: 'partials/likeable', locals: {link: @link} }
+    end
+  end
   private
 
   def params_method
